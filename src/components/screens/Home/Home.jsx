fd6363c78a0e6a9d42c343b308e9871d42325eb4
MixFix7 <mashtalar95@gmail.com>
2023-06-13 18:10:37 +0300

    create TodoItem.jsx

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import TodoItem from './TodoItem';
+
+const todos = [
+  {
+    _id: '1',
+    title: 'Finish the essay collaboration',
+    isComleted: false,
+  },
+  {
+    _id: '2',
+    title: 'Read next chapter of the book',
+    isComleted: false,
+  },
+  {
+    _id: '3',
+    title: 'Complete react map of alarms in Ukraine',
+    isComleted: false,
+  },
+  {
+    _id: '4',
+    title: 'Give a food for cat',
+    isComleted: false,
+  },
+]
 
 const Home = () => {
   return (
-    <div onChange={() => console.log('hello world')}>
-      Home
+    <div className='bg-gray-900 h-screen text-white' onChange={() => console.log('hello world')}>
+      
+    {todos.map(todo => (
+      <TodoItem key={todo._id} todo={todo} />
+    ))}
+
     </div>
+  
   )
-}
+};
 
 export default Home
